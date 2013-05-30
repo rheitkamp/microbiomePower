4827f89efcfd7f31d32331d926839d179bf8c762
Rae Heitkamp <rae.heitkamp@gmail.com>
2013-05-30 16:11:25 -0400

    Add outline for AD loop

---
+++ Outline for AD loop.R
@@ -0,0 +1,24 @@
+#create simulated datapoints from a normal distribution
+
+Taxon1 <- c(42, 20)
+Taxon2 <- c(52, 3)
+Taxon3 <- c(32, 5)
+Taxon4 <- c(40, 30)
+
+total <- 100
+
+#grab a number from taxon1
+taxon1 <- #simulatednumber1
+#subtract from total, new total = difference
+total <- total - #simulatednumber1
+#grab a number from taxon2
+remainderTaxon2 <- #simulatednumber2
+#multiply by total, store 
+taxon2<-total*#simulatednumber
+#subtract from total, new total = difference
+total <- total - #simulated number
+  ...#until all named taxa are accounted for
+others <- total
+simSamp <- c(taxon1, taxon2, taxon3, taxon4, others)
+
+

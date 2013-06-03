fd9dfa7d9d5a0559c4cb239c353658056b925abc
Sylvia Cheng <sylvia.cheng07@gmail.com>
2013-06-03 15:56:46 -0400

    edited AD.R file

--- AD.R
+++ AD.R
@@ -26,7 +26,7 @@ numrow <- 100 ###Number of subjects/samples you want to generate
 numcol <- 5 ###Number of taxa
 ADControl <- matrix(nrow=numrow,ncol=numcol)
 ADControl <- data.frame(ADControl)
-colnames(ADControl) <- c("Firmicutes", "Actinobacteria", "Proteobacteria", "Bacteroidetes", "Others")
+colnames(ADControl) <- c("Firmicutes", "Actinobacteria", "Proteobacteria", "Bacteroidetes", "Others") ###Names of columns
 size <- dim(ADControl)[1]
 
 for (i in 1:size){
@@ -117,45 +117,104 @@ ActinoFNT <- as.numeric(ADFlaresNT[2,])
 ProteoFNT <- as.numeric(ADFlaresNT[3,])
 BacterFNT <- as.numeric(ADFlaresNT[4,])
 
-###Fitting distribution 0-1
-unlist(pearsonFitML(FirmFNT))
-unlist(pearsonFitML(ActinoFNT))
-unlist(pearsonFitML(ProteoFNT))
-unlist(pearsonFitML(BacterFNT))
+###Distribution parameters 0-1 (proportions)
+FirmFNTparam <- pearsonFitML(FirmFNT)
+ActinoFNTparam <- pearsonFitML(ActinoFNT)
+ProteoFNTparam <- pearsonFitML(ProteoFNT)
+BacterFNTparam <- pearsonFitML(BacterFNT)
 
-###Fitted distribution taxa generation
 
-FirmicutesFNT <- rpearsonI(n=1000, a=0.1166093, b=0.1248777, location=0.5898664, scale=0.3871436)
-ActinobacteriaFNT <- rpearsonI(n=1000, a=0.81383910,  b=0.34911813, location=-0.02031676, scale=0.73061272)
-ProteobacteriaFNT <- rpearsonII(n=1000, a=0.4902809, location=0.1733658, scale=0.6121342)
-BacteroidetesFNT <- rpearsonI(n=1000, a=0.4347489, b=0.2513799, location=0.1557466, scale=0.8442535)
+###Histogram of taxa distributions
+hist(rpearson(n=1000, params=FirmFNTparam), breaks=100)
+hist(rpearson(n=1000, params=ActinoFNTparam), breaks=100)
+hist(rpearson(n=1000, params=ProteoFNTparam), breaks=100)
+hist(rpearson(n=1000, params=BacterFNTparam), breaks=100)
 
-###Histogram of generated taxa 0-1
-hist(FirmicutesFNT, 100)
-hist(ActinobacteriaFNT, 100)
-hist(ProteobacteriaFNT, 100)
-hist(BacteroidetesFNT, 100)
+###Generates AD Control dataset (proportions)###
+numrow <- 100 ###Number of subjects/samples you want to generate
+numcol <- 5 ###Number of taxa
+ADFNT <- matrix(nrow=numrow,ncol=numcol)
+ADFNT <- data.frame(ADFNT)
+colnames(ADFNTl) <- c("Firmicutes", "Actinobacteria", "Proteobacteria", "Bacteroidetes", "Others") ###Names of columns
+size <- dim(ADFNT)[1]
 
-###0-100 AD Flare No Treatment extracted individual taxa dataset
+for (i in 1:size){
+  total <- 1
+  ###Generate number for Firmicutes
+  ADFNT$Firmicutes[i] <- rpearson(n=1, params=FirmFNTparam)
+  total <- total - ADFNT$Firmicutes[i]
+  
+  ###Generate number for Actinobacteria
+  rActino <- rpearson(n=1, params=ActinoFNTparam)
+  ADFNT$Actinobacteria[i] <- (total*rActino)
+  total <- total - ADFNT$Actinobacteria[i]
+  
+  ###Generate number for Proteobacteria
+  rProteo <- rpearson(n=1, params=ProteoFNTparam)
+  ADFNT$Proteobacteria[i] <- (total*rProteo)
+  total <- total - ADFNT$Proteobacteria[i]
+  
+  ###Generate number for Bacteridetes
+  rBacter <- rpearson(n=1, params=BacterFNTparam)
+  ADFNT$Bacteroidetes[i] <- (total*rBacter)
+  
+  ###Remainder is placed in Others
+  ADFNT$Others[i] <- total - ADFNT$Bacteroidetes[i]
+  
+}
+
+###AD Flare No Treatment Loop
+library("PearsonDS")
+ADFlaresNT <- read.table("ADFlareNoTreatment.txt")
+
+###0-100 AD Flare No Treatment extracted individual taxa dataset (percentage)
 FirmFNT1 <- as.numeric(ADFlaresNT[1,])*100
 ActinoFNT1 <- as.numeric(ADFlaresNT[2,])*100
 ProteoFNT1 <- as.numeric(ADFlaresNT[3,])*100
 BacterFNT1 <- as.numeric(ADFlaresNT[4,])*100
 
-###Fitting distribution 0-100
-unlist(pearsonFitML(FirmFNT1))
-unlist(pearsonFitML(ActinoFNT1))
-unlist(pearsonFitML(ProteoFNT1))
-unlist(pearsonFitML(BacterFNT1))
+###Fitting distribution 0-100 (percentage)
+FirmFNTparam <- pearsonFitML(FirmFNT1)
+ActinoFNTparam <-pearsonFitML(ActinoFNT1)
+ProteoFNTparam <- pearsonFitML(ProteoFNT1)
+BacterFNTparam <- pearsonFitML(BacterFNT1)
 
-###Fitted distribution dataset generation
-FirmicutesFNT1 <- rpearsonII(n=1000, a=0.1835496, location=73.3078333, scale=24.3931667)
-ActinobacteriaFNT1 <- rpearsonI(n=1000, a=0.813595, b=0.346429, location=12.491984, scale=58.537612)
-ProteobacteriaFNT1 <- rpearsonI(n=1000, a=0.4793018,  b=0.6781994, location=22.6042533, scale=58.4019944)
-BacteroidetesFNT1 <- rpearsonI(n=1000, a=0.4361804, b=0.2796059, location=29.6034631, scale=70.3965525)
+###Histogram of taxa distributions
+hist(rpearson(n=1000, params=FirmFNTparam), breaks=100)
+hist(rpearson(n=1000, params=ActinoFNTparam), breaks=100)
+hist(rpearson(n=1000, params=ProteoFNTparam), breaks=100)
+hist(rpearson(n=1000, params=BacterFNTparam), breaks=100)
 
-###Histogram of generated taxa 0-100
-hist(FirmicutesFNT1, 100)
-hist(ActinobacteriaFNT1, 100)
-hist(ProteobacteriaFNT1, 100)
-hist(BacteroidetesFNT1, 100)
+numrow=100
+numcol=5
+ADFlareNT <- matrix(nrow=numrow,ncol=numcol)
+ADFlareNT <- data.frame(ADFlareNT)
+colnames(ADFlareNT) <- c("Firmicutes", "Actinobacteria", "Proteobacteria", "Bacteroidetes", "Others")
+size <- dim(ADFlareNT)[1]
+
+for (i in 1:size){
+  total <- 100
+  ###Generate number for Firmicute and place into table 
+  ADFlareNT$Firmicutes[i] <- rpearson(n=1, params=FirmFNTparam)
+  total <- total - ADFlareNT$Firmicutes[i]
+  
+  ###Generate number for Actinobacteria and place into table
+  rActinoFNT <- rpearson(n=1, params=ActinoFNTparam)
+  ADFlareNT$Actinobacteria[i] <- (total*rActinoFNT)/100
+  total <- total - ADFlareNT$Actinobacteria[i] 
+  
+  ###Generate number for Proteobacteria and place into table 
+  rProteoFNT <- rpearson(n=1, params=ProteoFNTparam)
+  ADFlareNT$Proteobacteria[i] <- (total*rProteoFNT)/100
+  total <- total - ADFlareNT$Proteobacteria[i]
+  
+  ###Generate number for Bacteroidetes and place into table
+  rBacterFNT <- rpearson(n=1, params=BacterFNTparam)
+  ADFlareNT$Bacteroidetes[i] <- (total*rBacterFNT)/100
+  
+  ###Remainder is placed into Others
+  ADFlareNT$Others[i] <- total - ADFlareNT$Bacteroidetes[i]
+  
+  
+}
+
